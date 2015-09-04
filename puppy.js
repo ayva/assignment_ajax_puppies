950aa6ec1fce4aa2aa4f15a64b4df9d038bc4877
ayva <olga.ayva@gmail.com>
2015-09-04 11:37:45 -0700

    initial setup

---
+++ puppy.js
@@ -0,0 +1,15 @@
+//Adding breeds from the list
+
+var Puppyshelter = {
+
+  var puppies = [];
+  var breeds = ["Poodle","Rottweiler","Sheepdog"];
+
+  for(var i=0; i<breeds.lenght; i++){
+    $('select').append('<option value="'+breed+'">'+breed+'</option>')  
+  }
+
+
+  //Set callback on button to add a puppies
+  //Set promise to add puppy in the list of puppies
+}
