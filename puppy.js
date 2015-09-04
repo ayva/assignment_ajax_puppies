05298b6fbc7945e75d33b5f434c918c64f8cafcd
mikebutts <mgbutts@gmail.com>
2015-09-04 15:00:59 -0400

    parsing the string

--- puppy.js
+++ puppy.js
@@ -1,15 +1,26 @@
 //Adding breeds from the list
-
+//https://pacific-stream-9205.herokuapp.com/puppies.json
 var Puppyshelter = {
 
   var puppies = [];
   var breeds = ["Poodle","Rottweiler","Sheepdog"];
 
-  for(var i=0; i<breeds.lenght; i++){
+  for(var i=0; i<breeds.length; i++){
     $('select').append('<option value="'+breed+'">'+breed+'</option>')  
-  }
+  } 
 
+  $.get("https://pacific-stream-9205.herokuapp.com/puppies.json",
+        )
 
   //Set callback on button to add a puppies
   //Set promise to add puppy in the list of puppies
 }
+
+var requestJson  = function(){
+      $.ajax( {
+        url: "https://pacific-stream-9205.herokuapp.com/puppies.json",
+        data: { },
+        type: "GET",
+        dataType : "json",
+      });
+    }
